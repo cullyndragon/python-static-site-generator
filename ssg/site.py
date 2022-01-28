c2a0c38a1569164551460df1a2c8da891c76e22a
Scott Kliemann <skliemann@wayfair.com>
2022-01-28 10:32:38 -0500

    Corrected unneeded underscores

--- ssg/site.py
+++ ssg/site.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        self._source = Path(source)
-        self._dest = Pathf(dest)
+        self.source = Path(source)
+        self.dest = Pathf(dest)
